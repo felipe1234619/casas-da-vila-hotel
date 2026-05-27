cf5cc5d2f8ef8229a77dd8cb357c117263b30210
Felipe Carvalho <felipe.carvalho@mac.com>
2026-05-27 15:45:55 -0300

    Fix site event CORS for analytics tracking

--- api/site-event.js
+++ api/site-event.js
@@ -50,7 +50,7 @@ export default async function handler(req, res) {
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
+res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     return res.status(204).end();
   }
